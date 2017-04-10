bb2b8125ff5ffe1f5a7f4e91d98d79f180f0c975
tcristi74 <tcristi74@yahoo.com>
2017-04-09 19:22:09 -0700

    done

--- main.go
+++ main.go
@@ -3,60 +3,91 @@ package main
 import (
 	"fmt"
 )
-//import "container/list"
+
+import (
+	"github.com/tcristi74/go-alg/tests"
+)
 
 var matrix [3][4]string
 
-//var dic *list.List
 var dic map[string]bool
+
+type direction string
 
 func init() {
 
-
-
 	// populate matrix
-
-	matrix[0][0]="d"
-	matrix[0][1]="d"
-	matrix[0][2]="e"
-	matrix[0][3]="d"
-
-	matrix[1][0]="e"
-	matrix[1][1]="d"
-	matrix[1][2]="e"
-	matrix[1][3]="d"
-
-	matrix[2][0]="c"
-	matrix[2][1]="m"
-	matrix[2][2]="r"
-	matrix[2][3]="w"
-
+	tests.LoadMatrix(&matrix)
 
 	// populate dictionary
-	dic = make(map[string]bool)
-	dic["de"] = true
-	dic["dec"] = true
-	dic["deca"] = true
-	dic["decal"] = true
-	dic["do"] = true
-	dic["dor"] = true
-	dic["di"] = true
-	dic["dir"] = true
-
-	//dic = list.New()
-	//dic.PushBack("de")1//dic.PushBack("dec")
-	//dic.PushBack("decal")
-	//dic.PushBack("dor")
-	//dic.PushBack("di")
-	//dic.PushBack("dir")
-
+	dic = tests.LoadDictionary()
 }
 
 func main() {
-	for key,value :=range dic {
+	for key, value := range dic {
 		fmt.Println("Key:", key, "Value:", value)
 	}
 	fmt.Println(matrix[0])
 	fmt.Println(matrix[1])
 	fmt.Println(matrix[2])
+
+
+
+	goodWords := make(map[string]bool)
+	for i := 0; i < len(matrix); i++ {
+		for v := 0; v < len(matrix[i]); v++ {
+			search(goodWords, [2]int{i, v}, "right", "")
+			search(goodWords, [2]int{i, v}, "left", "")
+			search(goodWords, [2]int{i, v}, "up", "")
+			search(goodWords, [2]int{i, v}, "down", "")
+		}
+	}
+	//print good words
+	fmt.Println("goodWords",goodWords)
+
+}
+
+func search(words map[string]bool, position [2]int, dir direction, prevStr string) {
+	//make sure the point exists
+	if !pointExists(matrix, position) {
+		return
+	}
+	// add new letter
+	prevStr += matrix[position[0]][position[1]]
+
+
+	// we can optimize this
+	if dic[prevStr]  && !words[prevStr]{
+		words[prevStr] = true
+	}
+	search(words, getNewPosition(position, dir), dir, prevStr)
+
+
+}
+
+func pointExists(matrix [3][4]string, position [2]int) bool {
+	//add code here
+	if position[0] < 0 || position[0] >= len(matrix) {
+		return false
+	}
+	if position[1] < 0 || position[1] >= len(matrix[0]) {
+		return false
+	}
+	return true
+}
+
+func getNewPosition(position [2]int, dir direction) [2]int {
+
+	newPosition := position
+	switch dir {
+	case "right":
+		newPosition[1]++
+	case "left":
+		newPosition[1]--
+	case "up":
+		newPosition[0]--
+	case "down":
+		newPosition[0]++
+	}
+	return newPosition
 }
---
+++ tests/tests.go
@@ -0,0 +1,75 @@
+package tests
+
+//import (
+//	"fmt"
+//	"testing"
+//)
+
+//LoadDictionary
+func LoadDictionary() map[string]bool {
+
+	dic := make(map[string]bool)
+
+	dic["de"] = true
+	dic["dec"] = true
+	dic["deca"] = true
+	dic["decal"] = true
+	dic["do"] = true
+	dic["dor"] = true
+	dic["di"] = true
+	dic["dir"] = true
+	dic["arm"] = true
+	dic["med"] = true
+	dic["ree"] = true
+	dic["re"] = true
+
+	return dic
+
+}
+
+//LoadMatrix
+func LoadMatrix(matrix  *[3][4]string){
+	matrix[0][0] = "d"
+	matrix[0][1] = "d"
+	matrix[0][2] = "e"
+	matrix[0][3] = "m"
+
+	matrix[1][0] = "e"
+	matrix[1][1] = "d"
+	matrix[1][2] = "e"
+	matrix[1][3] = "d"
+
+	matrix[2][0] = "c"
+	matrix[2][1] = "m"
+	matrix[2][2] = "r"
+	matrix[2][3] = "a"
+}
+
+//TestpointExists l
+//func TestpointExists(t *testing.T) {
+//
+//
+//
+//
+//	var matrix [3][4]string
+//
+//	LoadMatrix(matrix)
+//	result := pointExists(matrix, [2]int {2,3})
+//	fmt.Println("pointExists", result)
+//
+//	//fmt.Printf("Time elapsed QuickSort Concurrent: %d\n", timeQuickSort)
+//	//fmt.Printf("Time elapsed GenericSort: %d\n", timeGenericSorted)
+//
+//	fmt.Println("________end________")
+//}
+
+//TestgetNewPosition
+//func TestgetNewPosition(t *testing.T) {
+//	fmt.Println("pointExists")
+//
+//
+//	//fmt.Printf("Time elapsed QuickSort Concurrent: %d\n", timeQuickSort)
+//	//fmt.Printf("Time elapsed GenericSort: %d\n", timeGenericSorted)
+//
+//	fmt.Println("________end________")
+//}
